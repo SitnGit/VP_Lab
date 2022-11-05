2d2e5a553511da4030837187ecdef9ecb1f10a5e
Nikola Anastasovski <nikolaanastasovski54@gmail.com>
2022-11-06 00:20:16 +0100

    Lab1 FINAL

--- src/main/java/mk/ukim/finki/wp/lab/model/Balloon.java
+++ src/main/java/mk/ukim/finki/wp/lab/model/Balloon.java
@@ -5,15 +5,10 @@ import lombok.Data;
 public class Balloon {
     private String name;
     private String description;
-    private Long id;
 
-    private Manufacturer manufacturer;
-
-    public Balloon(String name, String description, Long id, Manufacturer manufacturer) {
+    public Balloon(String name, String description) {
         this.name = name;
         this.description = description;
-        this.id = id;
-        this.manufacturer = manufacturer;
     }
 
 }
